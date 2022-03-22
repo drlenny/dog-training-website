e0aa875c941a48e99f5e6659c95acc605633297e
drlenny <66442932+drlenny@users.noreply.github.com>
2022-03-22 14:20:54 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname + '/node_modules/bootstrap/dist')));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(express.json())
+
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html')
+})
+
+let port = process.env.PORT;
+
+if (port == null || port == "") {
+    port = 3000;
+}
+
+app.listen(port, function () {
+  console.log(`Server started on ${port} succesfully`);
+});
